622761bef7b23e5d48a7dafc83d4f97c2137594a
Douglas360 <douglas_henriqueduarte@hotmail.com>
2024-03-18 13:51:45 -0300

    Criar ICOTOkens

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,12 +4,12 @@ const Hero = ({ transferNativeToken }) => {
   return (
     <section
       data-settings="particles-1"
-      className="main-section curmina-flying-balls particles-js bg-1"
+      className="main-section crumina-flying-balls particles-js bg-1"
     >
       <div className="container">
         <div className="row medium-padding120 align-center">
           <div className="col-lg-8 col-lg-offset-2 col-md-12 col-sm-12 col-xs-12">
-            <header className="crumina-module curmina-heading heading--h2 heading--with-decoration">
+            <header className="crumina-module crumina-heading heading--h2 heading--with-decoration">
               <div className="heading-sup-title">Coin Market</div>
               <h2 className="heading-title heading--half-colored">
                 Created Liquidity Marketplace
--- components/ICOTokens.jsx
+++ components/ICOTokens.jsx
@@ -1,7 +1,121 @@
 import React from "react";
 
 const ICOTokens = () => {
-  return <div>ICOTokens</div>;
+  return (
+    <section className="medium-padding100">
+      <div className="container">
+        <div className="row align-center" id="started">
+          <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div className="crumina-module crumina-module-slider pagination-bottom-center">
+              <div
+                className="swiper-container"
+                data-show-items="3"
+                data-prev-next="1"
+              >
+                <div className="new_flex">
+                  {/*FIRST SWIPER */}
+                  <div className="swiper-slide">
+                    <div className="crumina-module crumina-pricing-table pricing-table--small">
+                      <div className="pricing-thumb">
+                        <img
+                          src="img/if_Bitcoin_2745023.png"
+                          alt=""
+                          className="woox-icon"
+                        />
+                        <h5 className="pricing-title">
+                          Bitcoin <span>BTC</span>
+                        </h5>
+                        <div className="gain-drop-arrow">
+                          <svg className="woox-icon icon-arrow-up arrow-up active">
+                            <use xlinkHref="#icon-arrow-up" />
+                          </svg>
+                          <svg className="woox-icon icon-arrow-up arrow-down active">
+                            <use xlinkHref="#icon-arrow-down" />
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="price">
+                        <div className="prive-sup-title">1 Bitcoin equals:</div>
+                        <div className="price-value">$94772.3</div>
+                        <div className="growth">+ 1.25%</div>
+                      </div>
+                      <a className="btn btn--large btn--datk-lighter btn--transparent full-width">
+                        Buy Bitcoin Now!
+                      </a>
+                    </div>
+                  </div>
+
+                  {/*SECOND SWIPER */}
+                  <div className="swiper-slide">
+                    <div className="crumina-module crumina-pricing-table pricing-table--small">
+                      <div className="pricing-thumb">
+                        <img
+                          src="img/if_etherium_eth_ethcoin_crypto_2844386.png"
+                          alt=""
+                          className="woox-icon"
+                        />
+                        <h5 className="pricing-title">
+                          Ethereum <span>ETH</span>
+                        </h5>
+                        <div className="gain-drop-arrow">
+                          <svg className="woox-icon icon-arrow-up arrow-up active">
+                            <use xlinkHref="#icon-arrow-up" />
+                          </svg>
+                          <svg className="woox-icon icon-arrow-up arrow-down active">
+                            <use xlinkHref="#icon-arrow-down" />
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="price">
+                        <div className="prive-sup-title">1 Bitcoin equals:</div>
+                        <div className="price-value">$4772.3</div>
+                        <div className="growth">+ 3.25%</div>
+                      </div>
+                      <a className="btn btn--large btn--datk-lighter btn--transparent full-width">
+                        Buy Ethereum Now!
+                      </a>
+                    </div>
+                  </div>
+
+                  {/*THIRD SWIPER */}
+                  <div className="swiper-slide">
+                    <div className="crumina-module crumina-pricing-table pricing-table--small">
+                      <div className="pricing-thumb">
+                        <img
+                          src="img/if_dash_dashcoin_2844383.png"
+                          alt=""
+                          className="woox-icon"
+                        />
+                        <h5 className="pricing-title">
+                          Dash <span>Dash</span>
+                        </h5>
+                        <div className="gain-drop-arrow">
+                          <svg className="woox-icon icon-arrow-up arrow-up active">
+                            <use xlinkHref="#icon-arrow-up" />
+                          </svg>
+                          <svg className="woox-icon icon-arrow-up arrow-down active">
+                            <use xlinkHref="#icon-arrow-down" />
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="price">
+                        <div className="prive-sup-title">1 Bitcoin equals:</div>
+                        <div className="price-value">$72.3</div>
+                        <div className="growth">+ 1.95%</div>
+                      </div>
+                      <a className="btn btn--large btn--datk-lighter btn--transparent full-width">
+                        Buy Dash Now!
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ICOTokens;
